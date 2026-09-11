7906b304b1b1a5fe070cd7eaeeeb36002349a88f
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2026-09-11 22:21:57 +0200

    Update App

---
+++ frontend/app/controllers/newsletter_controller.rb
@@ -0,0 +1,13 @@
+class NewsletterController < ApplicationController
+  before_action :set_email, only: %i[create]
+
+  def create
+    redirect_to home_path, notice: "Email added to newsletter successfully: %s" % @email
+  end
+
+  private
+
+  def set_email
+    @email = params[:email]
+  end
+end
