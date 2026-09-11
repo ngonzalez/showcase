2931880dab1a30d9f8ee4146e871e85dc9db32af
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2026-09-11 22:41:00 +0200

    Update App

--- frontend/config/routes.rb
+++ frontend/config/routes.rb
@@ -2,16 +2,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
+  # Pages
   get "home" => "pages#home", as: "home"
   get "register" => "pages#register", as: "register"
   get "plans" => "pages#plans", as: "plans"
 
   # Newsletter
-  post "/newsletter" => "newsletter#create"
+  post "newsletter" => "newsletter#create", as: "newsletter"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
