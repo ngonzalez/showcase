f203b09b2db75a5e18c1c67158c7d39a47befe41
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2026-09-11 21:45:50 +0200

    Update App

--- frontend/app/controllers/pages_controller.rb
+++ frontend/app/controllers/pages_controller.rb
@@ -1,7 +1,18 @@
 class PagesController < ApplicationController
+  before_action :set_plan, only: %i[register]
+
   def home
   end
+
+  def plans
+  end
+
   def register
-    
+  end
+
+  private
+
+  def set_plan
+    @plan = ["5GB", "10GB"].include?(params[:plan]) ? params[:plan] : "5GB"
   end
 end
