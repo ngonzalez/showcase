78fa816593a5aea4890a778fd2d931ba28e67295
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2026-09-13 03:56:56 +0200

    Update App

--- frontend/app/javascript/controllers/register_controller.js
+++ frontend/app/javascript/controllers/register_controller.js
@@ -14,16 +14,12 @@ export default class extends Controller {
   }
 
   input(event) {
-    _.each(['firstName', 'lastName', 'emailAddress', 'address', 'country'], (attribute, index) => {
+    _.each(['firstName', 'lastName', 'emailAddress', 'address', 'country', 'password', 'passwordConfirmation'], (attribute, index) => {
       if (event.target.name == "user[" + attribute + "]") {
         this.user[attribute] = event.target.value
       }
     })
-  }
-
-  submit(event) {
-    event.preventDefault()
-    console.log(this.user)
+    this.validateForm()
   }
 
   inputChanged(event) {
@@ -32,5 +28,27 @@ export default class extends Controller {
         this.user[attribute] = event.target.checked
       }
     })
+    this.validateForm()
+  }
+
+  validateForm() {
+    this.valid = true
+    _.each(['firstName', 'lastName', 'emailAddress', 'address', 'country', 'password', 'passwordConfirmation', 'agreeToTermsAndConditions'], (attribute, index) => {
+      if (typeof this.user[attribute] == "undefined" || this.user[attribute] == false) {
+        this.valid = false
+      }
+    })
+    if (this.valid) {
+      this.registerFormTarget.querySelectorAll('[name="submitButton"]')[0].classList.add('btn-neutral')
+    } else {
+      this.registerFormTarget.querySelectorAll('[name="submitButton"]')[0].classList.remove('btn-neutral')
+    }
+  }
+
+  submit(event) {
+    event.preventDefault()
+    if (this.valid) {
+      console.log(this.user)
+    }
   }
 }
