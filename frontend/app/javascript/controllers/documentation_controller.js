5ac63ba3dc79cf0a73a84d2836f40c4ea1e4ca7c
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2026-09-20 00:29:42 +0200

    Update App

--- frontend/app/javascript/controllers/documentation_controller.js
+++ frontend/app/javascript/controllers/documentation_controller.js
@@ -6,11 +6,34 @@ export default class extends Controller {
 
   connect() {
     _.each(document.getElementById('documentationMenu').querySelectorAll('li.menu-link'), (element, index) => {
+      element.classList.remove('active')
+
+      let statusElement = element.querySelectorAll('[aria-label="status"]')[0]
+      if (typeof statusElement != 'undefined') {
+        statusElement.style.visibility = 'hidden'
+      }
+
+      let statusAnimatedElement = element.querySelectorAll('[aria-label="status-animated"]')[0]
+      if (typeof statusAnimatedElement != 'undefined') {
+        statusAnimatedElement.style.visibility = 'hidden'
+      }
+    })
+    _.each(document.getElementById('documentationMenu').querySelectorAll('li.menu-link'), (element, index) => {
       let link = element.querySelectorAll('a')[0]
       if ((document.location.hash != '') && (typeof(link) != 'undefined') && (link.getAttribute('href') == document.location.hash)) {
         element.classList.add('active')
         let section = this.documentationPageTarget.querySelectorAll('[name="section"]')[0]
         section.innerHTML = element.getElementsByTagName('a')[0].getAttribute('href')
+
+        let statusElement = element.querySelectorAll('[aria-label="status"]')[0]
+        if (typeof statusElement != 'undefined') {
+          statusElement.style.visibility = 'visible'
+        }
+
+        let statusAnimatedElement = element.querySelectorAll('[aria-label="status-animated"]')[0]
+        if (typeof statusAnimatedElement != 'undefined') {
+          statusAnimatedElement.style.visibility = 'visible'
+        }
       }
     })
   }
@@ -18,9 +41,29 @@ export default class extends Controller {
   clickSidebarMenu(event) {
     _.each(document.getElementById('documentationMenu').querySelectorAll('li.menu-link'), (element, index) => {
       element.classList.remove('active')
+
+      let statusElement = element.querySelectorAll('[aria-label="status"]')[0]
+      if (typeof statusElement != 'undefined') {
+        statusElement.style.visibility = 'hidden'
+      }
+
+      let statusAnimatedElement = element.querySelectorAll('[aria-label="status-animated"]')[0]
+      if (typeof statusAnimatedElement != 'undefined') {
+        statusAnimatedElement.style.visibility = 'hidden'
+      }
     })
     event.target.parentNode.classList.add('active')
-    let section = this.documentationPageTarget.querySelectorAll('[name="section"]')[0]
-    section.innerHTML = event.target.getAttribute('href')
+    let sectionElement = this.documentationPageTarget.querySelectorAll('[name="section"]')[0]
+    sectionElement.innerHTML = event.target.getAttribute('href')
+
+    let statusElement = event.target.querySelectorAll('[aria-label="status"]')[0]
+    if (typeof statusElement != 'undefined') {
+      statusElement.style.visibility = 'visible'
+    }
+
+    let statusAnimatedElement = event.target.querySelectorAll('[aria-label="status-animated"]')[0]
+    if (typeof statusAnimatedElement != 'undefined') {
+      statusAnimatedElement.style.visibility = 'visible'
+    }
   }
 }
