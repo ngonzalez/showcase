7e4fa19f5bf97a7ec4555df6941a5003fab3effa
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2026-09-21 16:40:33 +0200

    Update App

--- frontend/config/application.rb
+++ frontend/config/application.rb
@@ -21,7 +21,8 @@ module Frontend
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Europe/Paris"
+
+    config.hosts << "#{APP_HOSTNAME}:#{APP_PORT}"
   end
 end
