f7b4b5c34d75a4c4c277d2e11f10dc098411dbfd
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2026-09-26 11:24:53 +0200

    Update App

--- frontend/config/importmap.rb
+++ frontend/config/importmap.rb
@@ -3,6 +3,6 @@
 pin "application"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "lodash" # @4.18.1
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "lodash" # @4.18.1
+pin_all_from "app/javascript/controllers", under: "controllers"
