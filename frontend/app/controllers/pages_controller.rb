a7923eaea3735add0803f44ca2631fd69c8d85e0
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2026-09-26 17:28:10 +0200

    Update App

--- frontend/app/controllers/pages_controller.rb
+++ frontend/app/controllers/pages_controller.rb
@@ -1,32 +1,47 @@
 class PagesController < ApplicationController
+  before_action :set_permitted_params, only: %i[register validate]
+  before_action :decode_user_payload, only: %i[register validate]
   before_action :set_plan, only: %i[register]
-  before_action :set_permitted_params, only: %i[validate]
-  before_action :decode_user_payload, only: %i[validate]
-  
+
   attr_accessor :permitted_params
   attr_accessor :user_payload
 
   def home
+    render("pages/home")
   end
 
   def plans
+    render("pages/plans")
   end
 
   def register
+    render("pages/register",
+      locals: {
+        user: {
+          payload: user_payload
+        }
+      }
+    )
   end
 
   def validate
     if permitted_params[:payload].nil?
       redirect_to(register_path)
     else
-      render("pages/validate", locals: { user: { payload: user_payload } })
+      render("pages/validate",
+        locals: {
+          user: {
+            payload: user_payload
+          }
+        }
+      )
     end
   end
 
   private
 
   def set_permitted_params
-    @permitted_params = params[:user].permit!
+    @permitted_params = params[:user].permit! if params[:user]
   end
 
   def decode_user_payload
@@ -34,6 +49,6 @@ class PagesController < ApplicationController
   end
 
   def set_plan
-    @plan = ["5GB", "10GB"].include?(params[:plan]) ? params[:plan] : "5GB"
+    @plan = ["5GB", "10GB"].include?(user_payload[:plan]) ? user_payload[:plan] : "5GB"
   end
 end
--- frontend/app/controllers/registration_controller.rb
+++ frontend/app/controllers/registration_controller.rb
@@ -1,22 +1,22 @@
 class RegistrationController < ApplicationController
-  before_action :set_permitted_params, only: %i[create]
+  before_action :set_permitted_params, only: %i[index create]
   before_action :encode_user_payload, only: %i[create]
 
   attr_accessor :permitted_params
   attr_accessor :user_payload
 
   def index
-    redirect_to(register_path)
+    redirect_to(register_path(user: { payload: user_payload }))
   end
 
   def create
-    redirect_to(validate_path(user: { payload: user_payload } ))
+    redirect_to(validate_path(user: { payload: user_payload }))
   end
 
   private
 
   def set_permitted_params
-    @permitted_params = params[:user].permit!
+    @permitted_params = params[:user].permit! if params[:user]
   end
 
   def encode_user_payload
